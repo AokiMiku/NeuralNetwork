815062fae015d48504100becef8e7a97ab5767dd
Melissa Menzel <mikemenzel77@yahoo.de>
2018-06-19 00:07:10 +0200

    added new EventArgs to feed forward the network in a different thread

---
+++ FeedForwardFinishedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+	public class FeedForwardFinishedEventArgs : EventArgs
+	{
+		public float[] Outputs { get; private set; }
+
+		public FeedForwardFinishedEventArgs(float[] outputs)
+		{
+			Outputs = outputs;
+		}
+	}
+}
